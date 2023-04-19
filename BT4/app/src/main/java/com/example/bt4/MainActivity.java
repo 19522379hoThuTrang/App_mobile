1f169fbbe1592fb7250475b3ae99d0167b0162db
19522379hoThuTrang <91778491+19522379hoThuTrang@users.noreply.github.com>
2023-04-19 15:07:15 +0700

    Add files via upload

--- BT4/app/src/main/java/com/example/bt4/MainActivity.java
+++ BT4/app/src/main/java/com/example/bt4/MainActivity.java
@@ -1,33 +1,38 @@
 package com.example.bt4;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText text;
+    private Button btn;
+    private EditText edt1,edt2,edt3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        text = findViewById(R.id.text);
-        final AlertDialog ad = new AlertDialog.Builder(this).create();
-        text.setOnKeyListener(new View.OnKeyListener() {
+        edt1 = findViewById(R.id.edt1);
+        edt2 = findViewById(R.id.edt2);
+        edt3 = findViewById(R.id.edt3);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                if(keyEvent.getAction() == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_DPAD_CENTER)
-                {
-                    String message = text.getText().toString();
-                    ad.setMessage(message);
-                    ad.show();
-                    return true;
-                }
-                return false;
+            public void onClick(View view) {
+                Intent iGet = new Intent(getApplicationContext(), ViewContactActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("name1", edt1.getText().toString());
+                bundle.putString("email1", edt2.getText().toString());
+
+                bundle.putString("project1", edt3.getText().toString());
+                iGet.putExtras(bundle);
+
+                startActivity(iGet);
+
             }
         });
     }
